ce8d5f31a803b09a01c0b4d4d63eed8bc89e4674
Unknown <prissstavko@gmail.com>
2018-12-19 22:02:43 +0500

    fixed arrays and resolved issues with variables checking

--- lab02/task1/main.cpp
+++ lab02/task1/main.cpp
@@ -5,26 +5,28 @@ using namespace std;
 
 int main()
 {cout<<"Input constants:\n";
+    int numl=1;
     char in[10];
-    int num[100];
+    int *num=new int[numl];
     int i,u;
 for(i=0;i<100;i++){
     for(u=0;u<10;u++){
     in[u]={0};
     };
     cin>>in;
-    if((in[0]=='e')&&(in[1]=='n')&&(in[2]=='d')&&(in[3]=='\0')){break;};
+    if((in[0]=='e')&&(in[1]=='n')&&(in[2]=='d')&&(in[3]=='\0'))
+        {break;};
     for(u=0;u<strlen(in);u++){
-         if(!(in[u]>='0')&&!(in[u]<='9')){
+         if(!((in[u]=='0')||(in[u]=='1')||(in[u]=='2')||(in[u]=='3')||(in[u]=='4')||(in[u]=='5')||(in[u]=='6')||(in[u]=='7')||(in[u]=='8')||(in[u]=='9'))){
             cout<<"Error! Invalid value. Try again if you dare, mortal!\n";
             for(int a=0;a<10;a++){
             in[u]={0};
-            break;
             };
             cin>>in;
-        }
+            }
     }
     num[i]=atoi(in);
+    numl++;
 };
 char *op=new char[i-2];
 int res=num[0];
@@ -56,6 +58,7 @@ for(int oi=0;oi<i-1;oi++){              //oi!
     };
 delete []op;
 }
+delete []num;
 cout<<"Result = "<<res;
 cin>>res;
 return 0;
